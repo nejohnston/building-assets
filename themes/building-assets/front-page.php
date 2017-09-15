21fb5eb81a4ee70a93e67f391a2759de655a5d98
njohnstud <nicholas.johnston94@gmail.com>
2017-09-15 11:29:17 -0700

    added fonts

--- themes/building-assets/front-page.php
+++ themes/building-assets/front-page.php
@@ -6,12 +6,15 @@
  */
 
 get_header(); ?>
-<section>
-    <div>
-	    <img class='hero' src="<?php echo get_template_directory_uri(); ?>/assets/images/hero-image.jpeg" alt="Image of Oppressed Burmese Women" />
-    </div>
-    <div class="hero-opacity">
-    </div>
-</section>
+<div class='content-wrapper'>
+    <section>
+        <div>
+            <img class='hero' src="<?php echo get_template_directory_uri(); ?>/assets/images/myanmar-kids.jpg" alt="Image of Oppressed Burmese Women" />
+        </div>
+            <h1 class='title'>Building Assets</h1>
+        <div class="hero-opacity">
+        </div>
+    </section>
+</div>
 
 		<?php wp_footer(); ?>
